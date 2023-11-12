f85e2bc56a4521672c38c6f3721b103dacaec36f
Mohith H B <mohithhb@gmail.com>
2023-11-12 17:45:28 +0530

    initial push

---
+++ react/ante-app/src/Constant.js
@@ -0,0 +1,33 @@
+export const BASE_URL = "http://localhost:3000/";
+
+function replaceUrl(imageUrl) {
+  imageUrl = imageUrl.replace(/\\/g, "/");
+  imageUrl = imageUrl.replace(/\//, "//");
+  return imageUrl;
+}
+
+export const miscUrl = "http://localhost:5000/api/miscs";
+
+export function miscImageCorrection(data) {
+  return data.map((newData) => {
+    return {
+      ...newData,
+      aboutBannerImageUrl: replaceUrl(newData.aboutBannerImageUrl),
+      blogListBannerImageUrl: replaceUrl(newData.blogListBannerImageUrl),
+      serviceListBannerImageUrl: replaceUrl(newData.serviceListBannerImageUrl),
+      contactUsBannerImageUrl: replaceUrl(newData.contactUsBannerImageUrl),
+    };
+  });
+}
+
+export function imageCorrection(data) {
+  if (data instanceof Array) {
+    return data.map((newData) => {
+      let imageUrl = newData.imageUrl;
+      return { ...newData, imageUrl: replaceUrl(imageUrl) };
+    });
+  } else {
+    let imageUrl = data.imageUrl;
+    return { ...data, imageUrl: replaceUrl(imageUrl) };
+  }
+}
